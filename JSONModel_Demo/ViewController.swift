abc09bf9b4fdbd050e7fdf73dcc4bd7ad4eff800
fengxiaohui <18367107969@163.com>
2017-04-01 15:44:11 +0800

    使用三方JSONKit转化数据

--- JSONModel_Demo/ViewController.swift
+++ JSONModel_Demo/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         
         
         // 使用三方类 JSONModel 获取JSON数据
-        //...
+        getWithJSONKit()
     }
 
     
@@ -40,26 +40,22 @@ class ViewController: UIViewController {
         let data = NSData(contentsOfFile: filePath) as! Data
         
         // 将data转化为string
-        let str = String(data: data, encoding: .utf8)!
-        print(str)
+        let string = String(data: data, encoding: .utf8)!
+        print(string)
         
         // 将data转化为JSON对象
         let jsonObject: AnyObject = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
-        print(jsonObject)
         
         // 使用JSON对象
-        let list: AnyObject = jsonObject.object(forKey: "list") as AnyObject
-        print(list)
+        let videoList: AnyObject = jsonObject.object(forKey: "videoList") as AnyObject
+        print("videoList:", videoList)
     }
     
     
     func getFromSwiftObject() {
         
         // 给定一个对象
-        let object = [
-            "userName": "xiaohui",
-            "tel": ["mobile": "186xxxx6666", "home": "666666"]
-        ] as [String : Any]
+        let object = ["userName": "xiaohui", "tel": ["mobile": "186xxxx6666", "home": "666666"]] as [String : Any]
         
         // 判断一个给定的对象是否可以转换为JSON数据
         guard JSONSerialization.isValidJSONObject(object) else {
@@ -71,19 +67,17 @@ class ViewController: UIViewController {
         let data = try! JSONSerialization.data(withJSONObject: object, options: []) as Data
         
         // 将data转化为string
-        let str = String(data: data, encoding: .utf8)!
-        print(str)
+        let string = String(data: data, encoding: .utf8)!
+        print(string)
         
         // 将data转化为JSON对象
         let jsonObject: AnyObject = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
-        print(jsonObject)
         
         // 使用JSON对象
         let userName: AnyObject = jsonObject.object(forKey: "userName") as AnyObject
-        print(userName)
         
         let mobile: AnyObject = (jsonObject.object(forKey: "tel") as AnyObject).object(forKey: "mobile") as AnyObject
-        print(mobile)
+        print("userName:", userName, "mobile:", mobile)
     }
     
     
@@ -102,9 +96,31 @@ class ViewController: UIViewController {
         let jsonArray = jsonObject as! NSArray
         
         for json in jsonArray {
-            print("name", (json as AnyObject).object(forKey: "name") ?? AnyObject.self)
-            print("age", (json as AnyObject).object(forKey: "age") ?? AnyObject.self)
+            print("name:", (json as AnyObject).object(forKey: "name") ?? AnyObject.self, "age:", (json as AnyObject).object(forKey: "age") ?? AnyObject.self)
         }
+    }
+    
+    
+    func getWithJSONKit() {
+        
+        // 给定一个对象
+        let object = ["userName": "xiaohui", "tel": ["mobile": "186xxxx6666", "home": "666666"]] as [String : Any]
+        
+        // 转化为JSON字符串
+        let string: String = (object as NSDictionary).jsonString() as String
+        print(string)
+        
+        // 反转化为字典
+        let dicWithString: NSDictionary = string.objectFromJSONString() as! NSDictionary
+        print(dicWithString)
+        
+        // 转化为JSON data
+        let data = (object as NSDictionary).jsonData() as NSData
+        print(data)
+        
+        // 反转化为字典
+        let dicWithData: NSDictionary = data.objectFromJSONData() as! NSDictionary
+        print(dicWithData)
     }
     
     override func didReceiveMemoryWarning() {
--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 target 'JSONModel_Demo' do
 
+#    pod 'JSONKit'
     pod 'JSONModel'
 
 end
